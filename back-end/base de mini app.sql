f289e81e848746cb5e2bdcc84c1342de89541af8
kevin alexander <no0336032020@unab.edu.sv>
2020-12-13 14:51:12 -0600

    actualizacion de la base de datos

--- back-end/base de mini app.sql
+++ back-end/base de mini app.sql
@@ -2,21 +2,28 @@
 use registroNotas;
 go
 
-create table Nota(
-Idmateria int primary key identity(1,1) not null,
-Nombre varchar(100)
-
-
+create table Notas(
+Idnota int primary key identity,
+Nota float,
+materia varchar(100),
 );
 
 create table Materias(
-Idnota int primary key identity(1,1) not null,
-Nombre varchar(100)
+IdMateria int primary key identity,
+materia varchar(100),
+profesor varchar(100),
+
 	
 );
 
-create table Estudiantes(
-Idnombre int primary key identity(1,1) not null,
-Nombre varchar(100)
-	);
+create table Estudiantes (
+Idnombre int primary key identity,
+Apellido1 varchar(100),
+Apellido2 varchar(100),
+);
+create table Profesor (
+idmombre int primary key identity,
+apellido varchar(100),
+apellido2 varchar(100),
 
+ );
