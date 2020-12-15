94ec912d5bb94702ef6405ce2692297dc4a04764
kevin alexander <no0336032020@unab.edu.sv>
2020-12-15 00:24:01 -0600

    nota avances

--- Notas/src/main/java/com/demo/Notas/DAO/NotaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.demo.Notas.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.demo.Notas.Entidades.Nota;
-
-public interface NotaDAO extends CrudRepository<Nota, Long>  {
-
-}
